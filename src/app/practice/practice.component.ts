0f3931a78fd39685966192676319a92a951b3b12
ModiSireesha <modisireesha09@gmail.com>
2024-10-16 22:17:21 +0530

    updated

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -11,4 +11,13 @@ export class PracticeComponent {
 
   title : string = "Hello Angular!"
   user = {name : "Sireesha" , age : 20}
+
+  imageUrl = "https://th.bing.com/th/id/OIP.CkvQxlvUO1XWC_G8cwrttgHaDr?rs=1&pid=ImgDetMain";
+  isButtonDisble = true;
+
+  message = " ";
+
+  onButtonClick() {
+    this.message = "Button is Clicked";
+  }
 }
