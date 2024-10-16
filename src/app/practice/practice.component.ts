299b05f9a2a75611e58742a84e03d782a6ce7d11
ModiSireesha <modisireesha09@gmail.com>
2024-10-16 21:59:29 +0530

    created practice

---
+++ src/app/practice/practice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice',
+  standalone: true,
+  imports: [],
+  templateUrl: './practice.component.html',
+  styleUrl: './practice.component.css'
+})
+export class PracticeComponent {
+
+}
