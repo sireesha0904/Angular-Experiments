277c059519d490884938a9697e6be1f2ccde3b22
ModiSireesha <modisireesha09@gmail.com>
2024-10-16 19:35:56 +0530

    created addition component

---
+++ src/app/addition/addition.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addition',
+  standalone: true,
+  imports: [],
+  templateUrl: './addition.component.html',
+  styleUrl: './addition.component.css'
+})
+export class AdditionComponent {
+
+}
