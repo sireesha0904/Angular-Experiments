63d7a6f7863e02b2c082d38f9af79bcd8e19731e
ModiSireesha <modisireesha09@gmail.com>
2024-10-16 19:41:55 +0530

    updated addition

--- src/app/addition/addition.component.ts
+++ src/app/addition/addition.component.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './addition.component.css'
 })
 export class AdditionComponent {
+  result: number | string = '';
+  calculate(operation: string, num1: string, num2: string) {
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
 
+    switch (operation) {
+      case 'add':
+        this.result = number1 + number2;
+        break;
+      case 'subtract':
+        this.result = number1 - number2;
+        break;
+      case 'multiply':
+        this.result = number1 * number2;
+        break;
+      case 'divide':
+        if (number2 !== 0) {
+          this.result = number1 / number2;
+        } else {
+          this.result = 'Cannot divide by zero';
+        }
+        break;
+      default:
+        this.result = 'Invalid operation';
+    }
+  } 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { NumberCheckerComponent } from './number-checker/number-checker.componen
 import { TextTogglerComponent } from './text-toggler/text-toggler.component';
 import { StudentDetailsComponentComponent } from './student-details-component/student-details-component.component';
 import { LoginCheckComponent } from "./login-check/login-check.component";
+import { AdditionComponent } from "./addition/addition.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormComponent, MenuComponent, LoginComponent, NumberCheckerComponent, TextTogglerComponent, StudentDetailsComponentComponent, LoginCheckComponent], 
+  imports: [FormComponent, MenuComponent, LoginComponent, NumberCheckerComponent, TextTogglerComponent, StudentDetailsComponentComponent, LoginCheckComponent, AdditionComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
