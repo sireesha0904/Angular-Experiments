4ed0dadb37c329e92e18f23dc55e782e7302712d
ModiSireesha <modisireesha09@gmail.com>
2024-10-22 16:25:13 +0530

    created service

---
+++ src/app/logi/logi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logi',
+  standalone: true,
+  imports: [],
+  templateUrl: './logi.component.html',
+  styleUrl: './logi.component.css'
+})
+export class LogiComponent {
+
+}
