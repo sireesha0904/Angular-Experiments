4492b1a2d06f6149459804dc77a662566731d6f0
ModiSireesha <modisireesha09@gmail.com>
2024-10-22 16:57:35 +0530

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule,RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
