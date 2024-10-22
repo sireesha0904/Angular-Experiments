f62245630d874b971dc1f5eb6a0fc7b9df9b4122
ModiSireesha <modisireesha09@gmail.com>
2024-10-22 10:58:18 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component'; // Adjust the path based on your structure
-import { AboutComponent } from './about/about.component'; // Adjust the path based on your structure
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent }, // This should route to HomeComponent
+  { path: 'about', component: AboutComponent }, // This routes to AboutComponent
 ];
 
 @NgModule({
