8bb0acfafdcdc5596d0f4148a2fca5bfde969760
ModiSireesha <modisireesha09@gmail.com>
2024-11-02 20:45:01 +0530

    updated

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [],
   templateUrl: './item-list.component.html',
-  styleUrl: './item-list.component.css'
+  styleUrls: ['./item-list.component.css'],
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnInit, OnDestroy {
+  items: string[] = ['Item 1', 'Item 2', 'Item 3'];
 
+  constructor() {
+    console.log('Constructor: ItemListComponent is created.');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit: ItemListComponent initialized.');
+  }
+
+  addItem(): void {
+    this.items.push(`Item ${this.items.length + 1}`);
+    console.log('Item added:', this.items[this.items.length - 1]);
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy: ItemListComponent is being destroyed.');
+  }
 }
