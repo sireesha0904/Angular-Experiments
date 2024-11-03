d30f667d6a25efaa6de7697b1a2ec656513480e8
ModiSireesha <modisireesha09@gmail.com>
2024-11-03 10:09:12 +0530

    updated

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,21 @@
-import { Component,Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ContentChild, AfterContentInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   standalone: true,
-  imports: [],
   templateUrl: './child.component.html',
-  styleUrl: './child.component.css'
+  styleUrls: ['./child.component.css']
 })
-export class ChildComponent {
-  @Input() childMessage: string = '';
-  @Output() messageEvent = new EventEmitter<string>();
+export class ChildComponent implements AfterContentInit {
+  @Input() childMessage: string = ''; 
+  @Output() messageEvent = new EventEmitter<string>(); 
+
+  @ContentChild('projectedContent') projectedContent!: ElementRef; 
+
+  ngAfterContentInit() {
+ 
+    console.log('Projected Content:', this.projectedContent.nativeElement.textContent);
+  }
 
   sendMessageToParent() {
     this.messageEvent.emit('Message from Child');
