5c969c335095b0d71f721db53660e0e4c140bb54
ModiSireesha <modisireesha09@gmail.com>
2024-11-03 10:14:26 +0530

    updated view child

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { ChildComponent } from '../child/child.component';
+
 @Component({
   selector: 'app-parent',
   standalone: true,
   imports: [ChildComponent],
   templateUrl: './parent.component.html',
-  styleUrl: './parent.component.css'
+  styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements AfterViewInit {
   parentMessage: string = 'Message from Parent to Child';
   messageFromChild: string = '';
 
+  @ViewChild(ChildComponent) childComponent!: ChildComponent; // Using ViewChild to access ChildComponent
+
+  ngAfterViewInit() {
+    // Access child's method after view initialization
+    const childMessage = this.childComponent.childMessage;
+    console.log('Child Message:', childMessage);
+  }
+
+  // Method to receive message from child
   receiveMessage(event: string) {
     this.messageFromChild = event;
   }
