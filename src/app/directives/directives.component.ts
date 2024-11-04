738b1e23130ce05201259e0da2c9679d8f5489d1
ModiSireesha <modisireesha09@gmail.com>
2024-11-04 19:05:57 +0530

    updated

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-directives',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './directives.component.html',
   styleUrls: ['./directives.component.css']
-  
 })
 export class DirectivesComponent {
+  showProfile: boolean = true;
+  textcolor: string = "black";
+  userName: string = "Modi Sireesha";
+  userAge: number = 220101120146;
+  friends: string[] = ["Siri", "gayatri", "kishore"];
 
-  showDetails : boolean = true;
-
-  toggleDetails() {
-    this.showDetails =!this.showDetails;
+  toggleProfile() {
+    this.showProfile = !this.showProfile;
   }
- 
-  Students = ["Sireesha", "Gayatri", "Kishore"] 
-  textcolor : string = "blue";
 
-  changeColor(){
-    this.textcolor = this.textcolor === "blue" ? "red" : "green";
+  changeColor() {
+    this.textcolor = this.textcolor === "black" ? "purple" : "black";
   }
 }
